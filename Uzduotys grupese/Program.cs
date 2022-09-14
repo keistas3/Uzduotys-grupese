4f79efd25850bf1c37f4f59d011fa34d5ca55ba3
keistas3 <k.strimaitis@gmail.com>
2022-09-14 18:40:34 +0300

    Add project files.

---
+++ Uzduotys grupese/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+ * »	Užduotys:
+1. Suskaičiuoti Žemės rutulio plota (spindulys: 6371 km).  Rezultatą rašyti į kintamąjį.
+2. Suskaičiuoti Žemės rutulio tūrį(spindulys: 6371 km). Rezultatą  rašyti įkintamąjį.
+3. Suskaičiuoti stačiojo trikampio įžambinės(prieš statųjį kampą esantį stačiojo  trikampio kraštinė) ilgį,jei kitų kraštinių ilgiai yra 10 ir 15. Rezultatą rašyti į  kintamąjį.
+4. Suskaičiuoti užduotyse 1-3 gautų kintamųjų sumą ir vidurkį. Įekraną
+išvesti rezultatą, kaip pateikta pavyzdyje.
