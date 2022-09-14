1c29ef743db4a31a3f1166aa36d9a6e7e2d3f2cc
MndugsBuz <mindaugas.bauza@codeacademylt.onmicrosoft.com>
2022-09-14 19:12:11 +0300

    3 dalis

--- Uzduotys grupese/Program.cs
+++ Uzduotys grupese/Program.cs
@@ -5,4 +5,37 @@
 3. Suskaičiuoti stačiojo trikampio įžambinės(prieš statųjį kampą esantį stačiojo  trikampio kraštinė) ilgį,jei kitų kraštinių ilgiai yra 10 ir 15. Rezultatą rašyti į  kintamąjį.
 4. Suskaičiuoti užduotyse 1-3 gautų kintamųjų sumą ir vidurkį. Įekraną
 išvesti rezultatą, kaip pateikta pavyzdyje.
-sdsdf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/ * 3 dalis */
+
+double statinisA = 10;
+double statinisB = 15;
+
+Console.WriteLine("Staciojo trikampio statiniai: A=10, B =15");
+
+double statinisC = Math.Sqrt(statinisA * statinisA + statinisB * statinisB);
+Console.WriteLine("StatinisC: " + statinisC);
+
+
+
