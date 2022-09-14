6ad2090cbb6dcaf59de7dde4a366869a96a8c87d
keistas3 <k.strimaitis@gmail.com>
2022-09-14 18:42:35 +0300

    Uzduotys

--- Uzduotys grupese/Program.cs
+++ Uzduotys grupese/Program.cs
@@ -5,3 +5,4 @@
 3. Suskaičiuoti stačiojo trikampio įžambinės(prieš statųjį kampą esantį stačiojo  trikampio kraštinė) ilgį,jei kitų kraštinių ilgiai yra 10 ir 15. Rezultatą rašyti į  kintamąjį.
 4. Suskaičiuoti užduotyse 1-3 gautų kintamųjų sumą ir vidurkį. Įekraną
 išvesti rezultatą, kaip pateikta pavyzdyje.
+sdsdf
